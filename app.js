fd3528d49ed1b12d4f0f5ff2c9d240420c623905
JP <polanco.joseph1@gmail.com>
2019-11-14 12:40:46 -0500

    we can see layout

---
+++ app.js
@@ -0,0 +1,18 @@
+const morgan = require('morgan')
+const layout = require('./views/layout')
+const express = require('express')
+const app = express()
+
+app.use(morgan('dev'))
+// app.use('/main')
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: false}))
+app.get('/', (req, res, next) =>{
+    res.send(layout(''))
+    // res.redirect('/main')
+})
+const port = 3000
+app.listen(port, () =>{
+    console.log(`Local host ${port} is connecting!`)
+})
